7469078f230659a6470493ad2db911bae03b45bf
Thomas Higdon <thomas.p.higdon@gmail.com>
2013-02-17 13:42:29 -0500

    oh yea

--- source/60_akamai.sh
+++
@@ -1,8 +0,0 @@
-
-keychain ~/.ssh/{internal,external,deployed} ~/.ssh/id_rsa
-[ -z "$HOSTNAME" ] && HOSTNAME=`uname -n`
-[ -f $HOME/.keychain/$HOSTNAME-sh ] && \
-  . $HOME/.keychain/$HOSTNAME-sh
-[ -f $HOME/.keychain/$HOSTNAME-sh-gpg ] && \
-  . $HOME/.keychain/$HOSTNAME-sh-gpg
-
