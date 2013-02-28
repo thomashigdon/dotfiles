bf342ab00dab4706b8545cf459409f5525c79402
Ben Alman <EMAIL>
2013-02-28 01:11:56 -0500

    Filter folders

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 import keyring
+import re
+
+my_folders = [ 'INBOX', r"l\/.*", "Drafts", "Sent Items", "Deleted Items$" ]
 
 if keyring.backends.Gnome.Keyring().supported() != -1:
     keyring.set_keyring(keyring.backends.Gnome.Keyring())
@@ -11,12 +14,15 @@ def set_password(server, user, password):
 def get_password(server, user):
     return keyring.get_password("offlineimap:%s" % server, user)
 
+def myfilter(name):
+    for folder in my_folders:
+        if re.match(folder, name):
+            return True
+    return False
+
 def mysort(x, y):
-    import re
-    enforce = [ 'INBOX', r"l\.*", "Drafts", "Sent Items", "Deleted Items" ]
-    x_match = len(enforce)
-    y_match = len(enforce)
-    for i, item in enumerate(enforce):
+    x_match = y_match = len(my_folders)
+    for i, item in enumerate(my_folders):
         if re.match(str(item), str(x)):
             x_match = i
         if re.match(str(item), str(y)):
