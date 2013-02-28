e36ffc363a85eedf75505b3b7a0c294f0a18b4b5
Ben Alman <EMAIL>
2013-02-27 22:22:39 -0500

    more email stuff

--- .offlineimap.py
+++ .offlineimap.py
@@ -42,3 +42,19 @@ def get_password(server):
     keyring = Keyring("offlineimap", server, "imap")
     (username, password) = keyring.get_credentials()
     return password
+
+def mysort(x, y):
+    import re
+    enforce = [ 'INBOX', r"l\.*", "Drafts", "Sent Items", "Deleted Items" ]
+    x_match = len(enforce)
+    y_match = len(enforce)
+    for i, item in enumerate(enforce):
+        if re.match(str(item), str(x)):
+            x_match = i
+        if re.match(str(item), str(y)):
+            y_match = i
+
+    if x_match == y_match:
+        return cmp(x,y)
+    else:
+        return x_match - y_match
